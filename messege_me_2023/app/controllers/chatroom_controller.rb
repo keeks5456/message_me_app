553bbe1d88e096ce36a650af650a830b5a51ddf1
Ubuntu <ubuntu@ip-172-31-12-181.us-west-1.compute.internal>
2023-03-19 20:17:15 +0000

    feat(Controllers): adds chatrom, user, sessions

---
+++ messege_me_2023/app/controllers/chatroom_controller.rb
@@ -0,0 +1,6 @@
+class ChatroomController < ApplicationController
+    
+    def index 
+        puts "hello"
+    end
+end
---
+++ messege_me_2023/app/controllers/sessions_controller.rb
@@ -0,0 +1,6 @@
+class SessionsController < ApplicationController
+    
+    def login 
+        puts "login" 
+    end
+end
