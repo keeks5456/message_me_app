2dd8381273d67c31caa7f7263be50c6d1609a657
Ubuntu <ubuntu@ip-172-31-12-181.us-west-1.compute.internal>
2023-04-14 20:17:52 +0000

    feat(login):users can login

--- messege_me_2023/app/models/user.rb
+++ messege_me_2023/app/models/user.rb
@@ -1,5 +1,4 @@
 class User < ApplicationRecord
-    before_save :downcase_username
     
     validates :username, presence: true, uniqueness: {case_sensitive: false}, length: { minimum: 3, maximum: 25}
     validates :password, length: { in: 6..20 }
@@ -9,8 +8,4 @@ class User < ApplicationRecord
     has_secure_password
     
     private 
-    
-    def downcase_username
-        self.username = username.downcase
-    end
 end
