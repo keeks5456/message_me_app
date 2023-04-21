538d621258fd5154931fadfa74b503c4c9ec2163
Ubuntu <ubuntu@ip-172-31-12-181.us-west-1.compute.internal>
2023-04-21 17:07:38 +0000

    feat(Messages):user can send messages

--- messege_me_2023/app/controllers/chatroom_controller.rb
+++ messege_me_2023/app/controllers/chatroom_controller.rb
@@ -1,11 +1,14 @@
 class ChatroomController < ApplicationController
     before_action :require_user
     
-    def index 
-       @messages = Message.all
+    def index
+        @message = Message.new
+        @messages = Message.all
     end
     
-    def new 
-       
+    def create 
+      
     end
+    
+    
 end
