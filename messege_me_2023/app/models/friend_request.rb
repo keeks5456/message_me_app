5eca0c9906e2608140cc8e44e94451b52ea198f7
Ubuntu <ubuntu@ip-172-31-12-181.us-west-1.compute.internal>
2023-04-29 23:58:06 +0000

    friendship:model created

--- messege_me_2023/app/models/friend_request.rb
+++ messege_me_2023/app/models/friend_request.rb
@@ -1,4 +1,4 @@
-class Match < ApplicationRecord
+class FriendRequest < ApplicationRecord
     belongs_to :requestor, class_name: "User"
     belongs_to :matchee, class_name: "User"
     # has_many :chats, dependent: :destroy // add this later
--- messege_me_2023/app/models/user.rb
+++ messege_me_2023/app/models/user.rb
@@ -4,11 +4,15 @@ class User < ApplicationRecord
     
     validates :username, presence: true, uniqueness: {case_sensitive: false}, length: { minimum: 3, maximum: 25}
     validates :password, length: { in: 6..20 }
-    
+    # messages
     has_many :messages, dependent: :destroy
+    # friend request
     has_many :friend_requests_as_requestor, foreign_key :requestor_id, class_name: :FriendRequest
     has_many :friend_requests_as_receiver, foreign_key :receiver_id, class_name: :FriendRequest
-
+    # friendships
+    has_many :friendships
+    has_many :friendships, foreign_key: :friend_id
+    has_many :friends, through: :friendships
     
     
     private 
