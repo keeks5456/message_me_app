9cf0563171c38c51a2a639c1633dd66781a58dd3
Ubuntu <ubuntu@ip-172-31-12-181.us-west-1.compute.internal>
2023-07-17 18:27:27 +0000

    adds rooms

--- messege_me_2023/app/controllers/rooms_controller.rb
+++ messege_me_2023/app/controllers/rooms_controller.rb
@@ -1,6 +1,8 @@
 class RoomsController < ApplicationController
   # before_action :authenticate_user
   def index
+    @current_user = current_user
+    redirect_to '/signup' unless @current_user
     @rooms = Room.public_rooms
     @users = User.all_except(current_user)
     
