628428a50d3cc1b7094ef9292f3a11439c95b52b
Akiko Green <akikogreen08@gmail.com>
2023-07-26 04:24:16 +0000

    Adds show controller, structures index view

--- messege_me_2023/app/controllers/rooms_controller.rb
+++ messege_me_2023/app/controllers/rooms_controller.rb
@@ -7,12 +7,23 @@ class RoomsController < ApplicationController
     @users = User.except(current_user)
   end
   
+  def show
+    @current_user = current_user
+    @single_room = Room.find(params[:id])
+    @rooms = Room.public_rooms
+    @users = User.all_except(@current_user)
+    @room = Room.new
+    
+    render "index"
+  end
+  
   def new 
     @room = Room.new
   end
   
+  # come back to this
   def create
-    @room = Room.create(name: params("name"))
+    @room = Room.create(name: params["name"])
   end
   
 end
