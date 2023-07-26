511d640943045226f8490c7bb1359c2e79a9a80f
Akiko Green <akikogreen08@gmail.com>
2023-07-26 04:10:09 +0000

    Create room not working except in console

--- messege_me_2023/app/controllers/rooms_controller.rb
+++ messege_me_2023/app/controllers/rooms_controller.rb
@@ -5,6 +5,14 @@ class RoomsController < ApplicationController
     redirect_to '/signup' unless @current_user
     @rooms = Room.public_rooms
     @users = User.except(current_user)
-    
   end
+  
+  def new 
+    @room = Room.new
+  end
+  
+  def create
+    @room = Room.create(name: params("name"))
+  end
+  
 end
